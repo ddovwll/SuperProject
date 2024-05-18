a9e07877aa57a479553f01941641ccd8f0e09310
pPhoenixsS <arisha.sannikova@mail.ru>
2024-05-18 15:13:29 +0300

    попытка 2

--- SuperClient/obj/Debug/net8.0-windows/SuperClient.AssemblyInfo.cs
+++ SuperClient/obj/Debug/net8.0-windows/SuperClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5b3cf3fc7171ec022bf19b48d81151e80a1ad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
