44da821cd00ab3c75b0614ce67487ab66c401729
irinarudak <rudak.ira@list.ru>
2024-05-18 20:56:45 +0300

    измения с добавлением и удалением лайка

--- SuperClient/presenters/ILikedPostsPresenter.cs
+++ SuperClient/presenters/ILikedPostsPresenter.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using SuperClient.models;
-using SuperClient.views;
+﻿using SuperClient.models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,52 +7,10 @@ using System.Threading.Tasks;
 
 namespace SuperClient.presenters
 {
-    internal class ILikedPostsPresenter
+    internal interface ILikedPostsPresenter
     {
-        ILikedPostsView view;
-        string result;
-
-        public ILikedPostsPresenter(ILikedPostsView view)
-        {
-            this.view = view;
-        }
-
-        public string resultAuth { get => result; set => value = result; }
-
-        public async Task<List<Post>> LikedPosts() // все посты
-        {
-            using HttpClient httpClient = new HttpClient();
-
-            httpClient.DefaultRequestHeaders.Add("UserId", headers.header.userId.ToString());
-            httpClient.DefaultRequestHeaders.Add("SessionId", headers.header.sessionId.ToString());
-
-            var response = await httpClient.GetAsync("http://localhost:5221/likes");
-
-            int statusCode = (int)response.StatusCode;
-
-            switch (statusCode)
-            {
-                case 401:
-                    result = "отсутствует один из заголовков или id пользователя не соответствует id сессии";
-                    // Unauthorized 
-                    break;
-                case 200:
-                    // Получение данных из ответа
-                    var data = await response.Content.ReadAsStringAsync();
-
-                    // Десериализация JSON данных в объект List<Post>
-                    var posts = JsonConvert.DeserializeObject<List<Post>>(data);
-                    result = "ok";
-                    return posts;
-                // Другие возможные коды состояния...
-                default:
-                    result = "ошибка";
-                    // Обработка других кодов состояния
-                    break;
-            }
-
-            return null;
-        }
-
+        public Task<List<Post>> LikedPosts();
+        public Task DeleteLike(int postId);
+        public string resultAuth { get; set; }
     }
 }
